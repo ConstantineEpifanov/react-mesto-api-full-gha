9c1c951e5856a8c702e3db3be4859ab31f2b96e5
Constantine Epifanov <asbest911@gmail.com>
2023-05-30 14:50:39 +0300

    add logger cors

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config();
 const helmet = require('helmet');
+const cors = require('cors');
 const rateLimit = require('express-rate-limit');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const { handleErrors } = require('./middlewares/handleErrors');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const router = require('./routes/index');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, DB = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const app = express();
 
@@ -16,15 +19,25 @@ const limiter = rateLimit({
   max: 100, // можно совершить максимум 100 запросов с одного IP
 });
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+mongoose.connect(DB);
 
 app.use(helmet());
+app.use(requestLogger);
+app.use(cors());
+app.options('/', cors());
 app.use(limiter);
 app.use(cookieParser());
 app.use(express.json());
 
-app.use('/', router);
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 
+app.use(router);
+
+app.use(errorLogger);
 app.use(errors());
 app.use(handleErrors);
 
